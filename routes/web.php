6a84da87f3db9c67d8295d51b1159efd6f74ab0e
sandeepa97 <lbsandeepa@gmail.com>
2021-09-10 00:36:37 +0530

    login form done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/admin', [AdminController::class, 'index']);
